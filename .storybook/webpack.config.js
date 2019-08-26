44a3a5a2d07167a3bb6b1e9ced7b97ea3739c301
Jean Abayo <abagammy@gmail.com>
2019-08-26 17:12:32 +0200

    fix storybook styles

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,48 +1,50 @@
-module.exports = ({
-	config
-}) => {
-	config.module.rules.push({
-		test: /\.(ts|tsx)$/,
-		use: [{
-				loader: require.resolve('awesome-typescript-loader'),
-				options: {
-					configFileName: './.storybook/tsconfig.json'
+module.exports = ({ config }) => {
+	config.module.rules.push(
+		{
+			test: /\.(ts|js)x?$/,
+			exclude: /node_modules/,
+			use: [
+				{
+					loader: "babel-loader"
+				},
+				// Optional
+				{
+					loader: require.resolve("react-docgen-typescript-loader")
 				}
-			},
-			// Optional
-			{
-				loader: require.resolve('react-docgen-typescript-loader'),
-			},
-		],
-	}, {
-		test: /\.css$/,
-		use: ['style-loader', 'css-loader'],
-	}, {
-		test: /\.scss$/,
-		use: [{
-				loader: 'style-loader'
-			},
-			{
-				loader: "css-modules-typescript-loader"
-			},
-			{
-				loader: 'css-loader',
-				options: {
-					importLoaders: 1,
-					modules: {
-						localIdentName: '[name]__[local]--[hash:base64:5]',
-					},
-					sourceMap: true
+			]
+		},
+		{
+			test: /\.css$/,
+			use: ["style-loader", "css-loader"]
+		},
+		{
+			test: /\.scss$/,
+			use: [
+				{
+					loader: "style-loader"
+				},
+				{
+					loader: "css-modules-typescript-loader"
+				},
+				{
+					loader: "css-loader",
+					options: {
+						importLoaders: 1,
+						modules: {
+							localIdentName: "[name]__[local]--[hash:base64:5]"
+						},
+						sourceMap: true
+					}
+				},
+				{
+					loader: "sass-loader",
+					options: {
+						sourceMap: true
+					}
 				}
-			},
-			{
-				loader: 'sass-loader',
-				options: {
-					sourceMap: true
-				}
-			}
-		]
-	});
-	config.resolve.extensions.push('.ts', '.tsx');
+			]
+		}
+	);
+	config.resolve.extensions.push(".ts", ".tsx");
 	return config;
 };
