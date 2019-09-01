6dc1938c2c95fa98f45e22ad5629d2f5f19f977a
Tosmak <tosmak16@gmail.com>
2019-09-01 07:33:51 +0100

    Add some icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,5 +4,9 @@
 // We are replacing these icons with SVG
 
 import brandLogo from  './icons/brandLogo.svg';
+import dashboard from  './icons/dashboard.svg';
+import inventory from  './icons/inventory.svg';
+import chevronDown from  './icons/chevronDown.svg';
+import chevronUp from  './icons/chevronUp.svg';
 
-export { brandLogo };
+export { brandLogo, dashboard, inventory, chevronDown, chevronUp };
